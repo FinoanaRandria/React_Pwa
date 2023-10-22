2981e6a562f51aaaafa21bd11b2ec59bf1ab20e4
FinoanaRandria <fitiavanarex4@gmail.com>
2023-10-22 21:28:16 +0300

    add map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPokemonList } from './api/pokemon';
 
 function App() {
+
+  const [pokemonData, setPokemonData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getPokemonList()
+      console.log(data);
+      setPokemonData(data?.result)
+    }
+
+    fetchData()
+  }, [])
+
+
   return (
     <div className="App">
-         <div>
-              
-         </div>
+      <div>
+        {
+          pokemonData.map((poke,i) => (
+                       <div>
+
+                         <img 
+                           style={{with:'300px',height:'300px'}}
+                           alt='pokemon'
+                           src={`https://img.pokemon.net/artwork/large/${poke.name}.jpg`}
+                         />
+
+
+                       </div>
+
+                    
+                   ))
+
+        }
+      </div>
 
     </div>
   );
