d8befa0964a69fc064fa42981434efab82f0f87a
FinoanaRandria <fitiavanarex4@gmail.com>
2023-10-22 21:12:07 +0300

    api

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      
+         <div>
+              
+         </div>
+
     </div>
   );
 }
---
+++ client/src/api/pokemon.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+
+
+const URL = `https://pokeapi.co/api/v2/pokemon?limit=151`
+
+
+export const getPokemonList = async ()=>{
+
+     const {data} = await axios.get(URL)
+
+     return data
+}
